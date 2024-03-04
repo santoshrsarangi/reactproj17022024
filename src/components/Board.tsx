6468c0a4e322bf1a3c52981355cdee1fabe990b7
Santosh Sarangi <santosh.sarangi@icloud.com>
2024-03-04 06:48:20 +0530

    Winner and game status updated

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,10 +5,38 @@ function Board() {
 
     const [btnValue, setBtnValue] = useState(Array(9).fill(null))
     const [selectedValue, setSelectedValue] = useState('X')
+    const winner = calculateWinner(btnValue);
+
+    let gameStatus;
+    if (winner) {
+        gameStatus = "Winner: " + winner;
+    } else {
+        gameStatus = "Next player: " + selectedValue;
+    }
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 
     function btnClicked(i: number) {
 
-        if (btnValue[i]) {
+        if (btnValue[i] || calculateWinner(btnValue)) {
             return;
         }
 
@@ -41,6 +69,8 @@ function Board() {
                 <Button label={btnValue[7]} handleClick={() => btnClicked(7)} />
                 <Button label={btnValue[8]} handleClick={() => btnClicked(8)} />
             </div>
+            <br></br>
+            <div className="game-status">{gameStatus}</div>
 
         </>
     )
